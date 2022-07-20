59144c5d81861e82d11a011925f191d1b0ccd130
Michael Avila <m.avilaarias@dxc.com>
2022-07-20 18:28:26 -0500

    cambiar icono y nombre de app

--- servidor/app/capacitor.config.ts
+++ servidor/app/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'edu.michalavila.app',
-  appName: 'app',
+  appName: 'petMonitor',
   webDir: 'dist/app',
   bundledWebRuntime: false,
   server: {
